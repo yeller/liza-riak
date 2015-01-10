a989a1ea39c965106c7e83fe08b3c1cd50adeb34
Tom Crayford <tcrayford@googlemail.com>
2015-01-10 03:23:50 +0000

    bump riak client dep

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [liza "0.1.0-SNAPSHOT"]
-                 [com.basho.riak/riak-client "1.4.2"]
+                 [com.basho.riak/riak-client "1.4.4"]
                  [metrics-clojure "1.0.1"]
                  [fressian-clojure "0.2.0"]
                  [org.xerial.snappy/snappy-java "1.1.1-M1"]])
