10c6c199dfeb4eac6479e5298a1a3cc72a137e42
Tom Crayford <tcrayford@googlemail.com>
2015-01-23 16:04:51 +0000

    bump version, bump liza version to stable

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject liza-riak "0.5.0-SNAPSHOT"
+(defproject liza-riak "0.5.0"
   :global-vars {*warn-on-reflection* true}
   :description "Abstract key/value store interface"
   :url "https://github.com/liza-riak"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [liza "0.1.0-SNAPSHOT"]
+                 [liza "0.1.0"]
                  [com.basho.riak/riak-client "1.4.4"]
                  [metrics-clojure "1.0.1"]
                  [fressian-clojure "0.2.0"]
